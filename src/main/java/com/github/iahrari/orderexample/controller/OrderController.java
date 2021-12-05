d9f43328100da29248cce3b6224b5f07d3b46e00
Iman Ahrari <iahrari@pm.me>
2021-12-05 21:29:28 +0330

    feat(controller): add order handler controller

---
+++ src/main/java/com/github/iahrari/orderexample/controller/OrderController.java
@@ -0,0 +1,36 @@
+package com.github.iahrari.orderexample.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.github.iahrari.orderexample.dto.OrderDTO;
+import com.github.iahrari.orderexample.repository.OrderRepository;
+import com.github.iahrari.orderexample.service.OrderService;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    
+    @PostMapping
+    public OrderDTO addOrder(@Valid @RequestBody OrderDTO orderDTO) 
+            throws MethodArgumentNotValidException{
+        return orderService.saveOrder(orderDTO);
+    }
+
+    @GetMapping
+    public List<OrderDTO> getAll(){
+        return orderService.getAllOrders();
+    }
+}
